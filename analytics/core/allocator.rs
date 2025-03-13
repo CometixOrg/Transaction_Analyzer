d09888bb826f7b5778abde5e51678500fd655f93
frankysoldev <reposedev7@gmail.com>
2025-03-13 08:01:24 +0000

    Added transaction allocator

---
+++ analytics/core/allocator.rs
@@ -0,0 +1,10 @@
+use transaction_analyzer_runtime::ledger::extension::FieldExtension;
+use solana_program::{account_info::AccountInfo, program_error::ProgramError};
+
+pub fn get_context_allocation(
+    _asset_info: &AccountInfo,
+    _protocol_info: &AccountInfo,
+    _extensions: &[FieldExtension],
+) -> Result<usize, ProgramError> {
+    Ok(165) // Placeholder
+}
